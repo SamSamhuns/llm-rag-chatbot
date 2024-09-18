ab6efe010e6df89cd64577543b01c6315f547249
SamSamhuns <samhunsadamant@gmail.com>
2024-09-18 11:07:55 +0400

    Update python pkg reqs & type hints

--- app/models/logging.py
+++ app/models/logging.py
@@ -16,13 +16,13 @@ from pydantic import BaseModel
 class LogConfig(BaseModel):
     """Logging configuration to be set for the server"""
 
-    INFO_FILE_PATH = "info.log"
-    ERROR_FILE_PATH = "error.log"
+    INFO_FILE_PATH: str = "info.log"
+    ERROR_FILE_PATH: str = "error.log"
 
     # Logging config
-    version = 1
-    disable_existing_loggers = False
-    formatters = {
+    version: int = 1
+    disable_existing_loggers: bool = False
+    formatters: dict = {
         'info': {
             "()": "uvicorn.logging.DefaultFormatter",
             "fmt": "%(levelprefix)s | %(asctime)s | %(name)s | %(message)s",
@@ -34,7 +34,7 @@ class LogConfig(BaseModel):
             "datefmt": "%Y-%m-%d %H:%M:%S",
         },
     }
-    handlers = {
+    handlers: dict = {
         'debug_console_handler': {
             'level': 'DEBUG',
             'formatter': 'info',
@@ -74,7 +74,7 @@ class LogConfig(BaseModel):
             'subject': 'Critical error with application name'
         },
     }
-    loggers = {
+    loggers: dict = {
         '': {  # root logger
             'level': 'NOTSET',
             'handlers': ['debug_console_handler', 'info_rotating_file_handler', 'error_file_handler', 'critical_mail_handler'],
